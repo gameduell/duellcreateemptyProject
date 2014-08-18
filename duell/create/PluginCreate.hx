cb0d59fa027ab636d283a129dde8fe9c43acc2cd
Sven Otto <sven.otto@gameduell.de>
2014-08-18 11:05:37 +0200

    Initial commit

---
+++ duell/create/PluginCreate.hx
@@ -0,0 +1,16 @@
+package duell.create;
+
+class PluginCreate
+{
+    public function new()
+    {
+
+    }
+
+    public function run(args : Array<String>) : String
+    {
+        trace("Copy emptyProject template here");
+
+        return "success";
+    }
+}
